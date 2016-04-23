966cbd56db168c15cebe6d61d0862a9f1d48a47f
Bryce Krah <krahbryce@yahoo.com>
2016-04-23 19:42:27 -0400

    parsing data with es7 Object.values for dashboard component view

--- App/Components/dashboard.js
+++ App/Components/dashboard.js
@@ -25,8 +25,19 @@ class Dashboard extends React.Component{
   render(){
     return (
       <View style={styles.container}>
-        <Text> This is the dashboard </Text>
-        <Text> {JSON.stringify(this.props.movieInfo)} </Text>
+        <Text> Title: {Object.values(this.props.movieInfo.Title)} </Text>
+        <Text> Year: {Object.values(this.props.movieInfo.Year)} </Text>
+        <Text> Rated: {Object.values(this.props.movieInfo.Rated)} </Text>
+        <Text> Released: {Object.values(this.props.movieInfo.Released)} </Text>
+        <Text> Genre: {Object.values(this.props.movieInfo.Genre)} </Text>
+        <Text> Director: {Object.values(this.props.movieInfo.Director)} </Text>
+        <Text> Writer: {Object.values(this.props.movieInfo.Writer)} </Text>
+        <Text> Actors: {Object.values(this.props.movieInfo.Actors)} </Text>
+        <Text> Plot: {Object.values(this.props.movieInfo.Plot)} </Text>
+        <Text> Poster: {Object.values(this.props.movieInfo.Poster)} </Text>
+        <Text> Awards: {Object.values(this.props.movieInfo.Awards)} </Text>
+        <Text> Metascore: {Object.values(this.props.movieInfo.Metascore)} </Text>
+        <Text> imdbRating: {Object.values(this.props.movieInfo.imdbRating)} </Text>
       </View>
     )
   }
